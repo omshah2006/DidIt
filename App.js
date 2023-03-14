3ac81b83dd39638a8fdd5bf0b839ff58ca4c0cfc
Rayhan <rayhan@live.com>
2023-03-13 22:05:11 -0700

    login sample

--- App.js
+++ App.js
@@ -1,55 +1,84 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image, ScrollView, Dimensions} from 'react-native';
-
+import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  TextInput,
+  Button,
+  TouchableOpacity,
+} from "react-native";
 export default function App() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   return (
     <View style={styles.container}>
-      <ScrollView style={styles.scrollContainer}>
-        <Image 
-          style={styles.image}
-          source={require('./assets/henryandom.jpg')} 
-        />
-        <Image 
-          style={styles.image}
-          source={require('./assets/henryandom.jpg')} 
-        />
-        <Image 
-          style={styles.image}
-          source={require('./assets/henryandom.jpg')} 
-        />
-        <Image 
-          style={styles.image}
-          source={require('./assets/henryandom.jpg')} 
-        />
-        <Image 
-          style={styles.image}
-          source={require('./assets/henryandom.jpg')} 
-        />
-        <Image 
-          style={styles.image}
-          source={require('./assets/henryandom.jpg')} 
-        />
-      </ScrollView>
-
       <StatusBar style="auto" />
-    </View>
+      <View style={styles.inputView}>
+        <TextInput
+          style={styles.TextInput}
+          placeholder="Email. "
+          placeholderTextColor="#fff"
+          onChangeText={(email) => setEmail(email)}
+        /> 
+      </View> 
+      <View style={styles.inputView}>
+        <TextInput
+          style={styles.TextInput}
+          placeholder="Password. "
+          placeholderTextColor="#fff"
+          secureTextEntry={true}
+          onChangeText={(password) => setPassword(password)}
+        /> 
+      </View> 
+      <TouchableOpacity>
+        <Text style={styles.forgot_button}>Forgot Password?</Text> 
+      </TouchableOpacity> 
+      <TouchableOpacity style={styles.loginBtn}>
+        <Text style={styles.loginText}>LOGIN</Text> 
+      </TouchableOpacity> 
+    </View> 
   );
 }
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#FFFDD0',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  scrollContainer: {
-    showsVerticalScrollIndicator: false,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
   image: {
-    height: 300, 
-    width: Dimensions.get('window').width,
-    marginTop: 50,
-    marginBottom: 50
-  }
+    marginBottom: 40,
+  },
+  inputView: {
+    backgroundColor: "#000000",
+    borderRadius: 30,
+    width: "70%",
+    height: 45,
+    marginBottom: 20,
+    alignItems: "center",
+  },
+  TextInput: {
+    height: 50,
+    flex: 1,
+    padding: 10,
+    marginLeft: 20,
+  },
+  forgot_button: {
+    height: 30,
+    marginBottom: 30,
+  },
+  loginBtn: {
+    width: "80%",
+    borderRadius: 25,
+    height: 50,
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: 40,
+    backgroundColor: "#000000",
+  },
+  loginText: {
+    color: 'white',
+  },
 });
