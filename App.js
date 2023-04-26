808a04689ef5467919e4ec2ff61a85d8d802e1f4
Om Shah <omshah2006@gmail.com>
2023-04-25 21:19:54 -0700

    removed ugly forehead on screens

--- App.js
+++ App.js
@@ -10,7 +10,10 @@ const Stack = createNativeStackNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
+      <Stack.Navigator
+        screenOptions={{
+          headerShown: false,
+        }}>
         <Stack.Screen 
           name="Signup"
           component={Signup}
